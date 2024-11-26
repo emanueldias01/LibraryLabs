04cfd93e2fc17a74149cdb788850febf984c5028
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-11-26 16:00:27 -0300

    trata caso de falha na conexao com banco

--- database/database.go
+++ database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,4 +14,8 @@ var(
 func ConnectDB(){
 	strConn := "host=localhost user=root password=root dbname=librarylabs port=9920 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(strConn))
+
+	if err != nil{
+		log.Fatal("fail to connect to the database")
+	}
 }

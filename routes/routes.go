998031cecdfb03877b8c227899a2ba577acc3286
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-11-26 16:23:53 -0300

    mapeia rotas

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"librarylabs/controllers"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,5 +11,9 @@ import (
 var r = mux.NewRouter()
 
 func HandleRequest(){
+	r.HandleFunc("/books", controllers.GetAllBooks)
+	r.HandleFunc("/books/create", controllers.CreateBook)
+	r.HandleFunc("/books/update/{id}", controllers.UpdateBook)
+	r.HandleFunc("/books/delete/{id}", controllers.DeleteBook)
 	http.ListenAndServe(":8000", r)
 }

9e2c104bf0c275888d87c4473724bf7a2cb408da
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-11-27 12:28:11 -0300

    refatoracao de funcoes para book

---
+++ models/bookMethots.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"librarylabs/database"
+)
+
+func GetAllBooks() []Book{
+	var list []Book
+	database.DB.Find(&list)
+
+	return list
+}
+
+func GetBookById(id int) Book{
+	var book Book
+	database.DB.First(&book, id)
+
+	return book
+}
+
+func CreateBook(bookCreate Book){
+	database.DB.Create(&bookCreate)
+}
+
+func UpdateBook(bodyBook Book, id int) Book{
+	//Find the book i want to update
+	var bookUpdate Book
+	database.DB.First(&bookUpdate, id)
+
+	//Change atributes book
+	if bodyBook.Name != "" {
+		bookUpdate.Name = bodyBook.Name
+	}
+
+	if bodyBook.Author != "" {
+		bookUpdate.Author = bodyBook.Author
+	}
+
+	if bodyBook.Description != "" {
+		bookUpdate.Description = bodyBook.Description
+	}
+
+	if bodyBook.Genre.String() != "" {
+		bookUpdate.Genre = bodyBook.Genre
+	}
+
+	//Save new changes
+	database.DB.Save(&bookUpdate)
+
+	return bookUpdate
+}
+
+func DeleteBook(id int){
+	var bookDelete Book
+	database.DB.Delete(&bookDelete, id)
+}
